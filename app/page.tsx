c830a27a6da1f44568724b0c64c37432de0fc200
Osman Sait Güngör <sait@bronixengineering.com>
2023-11-04 13:46:36 +0300

    basic suspense implemented

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Component from "@/components/Component";
-import React, { FC } from "react";
+import React, { FC, Suspense } from "react";
 
 const Page = () => {
   return (
     <div className="w-screen h-screen flex flex-col gap-4 justify-center items-center">
       <h1 className="text-2xl">This is static page content</h1>
-      <Component />
+      <Suspense fallback={<h2 className="text-2xl text-red-500">This is fallback </h2>}>
+        <Component />
+      </Suspense>
     </div>
   );
 };
--- components/Component/index.tsx
+++ components/Component/index.tsx
@@ -1,7 +1,17 @@
+"use client";
 import React, { FC } from "react";
 
 const Component: FC = () => {
-  return <div className="text-2xl text-blue-400">This is components data</div>;
+  async function waitForThreeSeconds() {
+    await new Promise((resolve) => setTimeout(resolve, 3000)); // Wait for 3 seconds
+    return "Done waiting after 3 seconds!"; // Update the state
+  }
+  const a = waitForThreeSeconds();
+  return (
+    <div className="text-2xl text-blue-400">
+      This is components data<h5>{a}</h5>
+    </div>
+  );
 };
 
 export default Component;
